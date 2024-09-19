2cab5f0388ee09e34ffeb83ee724fe21aef7caed
Andre Lucas <andrelucastic@gmail.com>
2024-09-19 14:01:28 +0100

    change to version - topics and queues

---
+++ src/main/java/io/andrelucas/fundamentalsofsolace333/ChatServer.java
@@ -0,0 +1,13 @@
+package io.andrelucas.fundamentalsofsolace333;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatServer.class, args);
+    }
+
+}
---
+++ src/test/java/io/andrelucas/fundamentalsofsolace333/ChatServerTests.java
@@ -0,0 +1,13 @@
+package io.andrelucas.fundamentalsofsolace333;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
